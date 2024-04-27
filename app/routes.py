1a33cbe87da4478676605f964a556e08cc75b791
Kory Plotts <koryplotts@yahoo.com>
2024-04-27 12:05:48 -0700

    112 class 1

--- app/routes.py
+++ app/routes.py
@@ -22,9 +22,9 @@ app = Flask(__name__)
 def about():
     return render_template("about.html")
 
-@app.get("tasks")
+@app.get("/tasks")
 def task_list():
-    response = request.get(BACKEND_URL)
+    response = requests.get(BACKEND_URL)
     if response.status_code == 200:
         task_list = response.json().get("tasks")
         return render_template("list.html", tasks=task_list)
@@ -36,11 +36,35 @@ def task_list():
 @app.get("/tasks/<int:pk>")
 def detail(pk):
     url = "%s/%s" % (BACKEND_URL, pk)
-    response = request.get(url)
+    response = requests.get(url)
     if response.status_code == 200:
         task = response.json().get("tasks")
         return render_template("detail.html", task=task)
     return (
+        render_template("error.html", err=response.status_code),
+        response.status_code
+    )
+
+
+@app.get("/tasks/edit/<int:pk>")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response= requests.get(url)
+    if response.status_code == 200:
+        task = response.json().get("task")
+        return render_template("edit.html", task=task)
+    return(
+        render_template("error.html", err=response.status_code),
+        response.status_code
+        )
+@app.post("/tasks/edit/<int:pk>")
+def submit_task_update(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    task_data = request.form
+    response = requests.put(url, json=task_data)
+    if response.status_code == 204:
+        return render_template("success.html", msg="Task updated successfully.")
+    return(
         render_template("error.html", err=response.status_code),
         response.status_code
     )
